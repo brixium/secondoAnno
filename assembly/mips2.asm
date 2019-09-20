2e1b2b19ca7eb04473a6d8aa70c79cdc6542bab7
Lorenzo <brixiumx@gmail.com>
2019-09-20 19:37:59 +0200

    removed redundant folder

--- assembly/mips2.asm
+++
@@ -1,5 +0,0 @@
-# a = (b+c)-(d-f)+g
-add $t0, $s1, $s2
-sub $t1, $s3, $s4
-sub $t0, $t0, $t1
-add $s3, $t0, $s5
--- assembly/mips3.asm
+++
@@ -1,9 +0,0 @@
-lw $t0, 100($s0)
-# carica la parola (load word) dalla cella di memoria di di indirizzo
-(num1 + $s0) in $t0
-sw $t0, 100($s0)
-# salva la parola (store word) in $t0 all'indirizzo (num1 + $s0)
-li $v0, 4
-# carica la costante 4 in $v0 (load immediate)
-la $a0, 100
-# carica l'indirizzo (load address) num1 in $a0
