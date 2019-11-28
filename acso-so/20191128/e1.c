b5179c3484bf337e98556aa28e1752e4a0554372
Lorenzo <brixiumx@gmail.com>
2019-11-28 15:59:04 +0100

    esercitazione di oggi

---
+++ acso-so/20191128/e1.c
@@ -0,0 +1,9 @@
+Si considerino due processi P e Q. La situazione iniziale considerata è la seguente:
+	il processo P è in esecuzione in modo U
+	il processo Q è in stato d'attesa dell'evento E. La coda RB è da considerarsi vuota (si ricorda che IDLE non appartiene alla RB)
+La figura riportata e i valori nella tabella successiva descrivono compiutamente, ai fini dell'esercizio, il contesto di P e quello di Q.
+Vignetta di Daniel Stori @ turnoff.us Bob e i suoi thread
+A parte la prima parte il resto è un cazzo di casino
+Vedi un foglietto volante per la soluzione (o meglio le slides del profe).
+Non ci si capisce un cazo lo stesso
+
